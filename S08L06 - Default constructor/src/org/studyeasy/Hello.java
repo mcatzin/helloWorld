4522ead7bb37d0b69198508c1d8e8787fc35c67e
mcatzin <catzin21@gmail.com>
2020-02-04 16:21:15 -0800

    First commit

---
+++ S08L06 - Default constructor/src/org/studyeasy/Hello.java
@@ -0,0 +1,11 @@
+package org.studyeasy;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Smartphone phone = new Smartphone();
+		System.out.println(phone.getBrand());
+	}
+
+}
---
+++ S08L06 - Default constructor/src/org/studyeasy/Smartphone.java
@@ -0,0 +1,21 @@
+package org.studyeasy;
+
+public class Smartphone {
+	private String brand = "Samsung";
+	
+	public Smartphone() {
+		
+	}
+
+	public Smartphone(String brand) {
+		super();
+		this.brand = brand;
+	}
+
+
+
+	public String getBrand() {
+		return brand;
+	}
+	
+}
